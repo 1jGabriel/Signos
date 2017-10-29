58495674f9bf56015fa2403dd88de55667d59175
1jGabriel <gsantana152@gmail.com>
2017-10-29 19:30:35 -0300

    first commit

---
+++ app/src/main/java/signos/android/projetos/com/signos/MainActivity.java
@@ -0,0 +1,13 @@
+package signos.android.projetos.com.signos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
